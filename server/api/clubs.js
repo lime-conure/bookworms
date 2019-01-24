6a82bf063fe255f685dca6276fcddcfcd8b03284
Norka Avalos <norka.avalos11@gmail.com>
2019-01-24 12:06:05 -0500

    feat: backend api route for voting and displaying single polls

--- server/api/clubs.js
+++ server/api/clubs.js
@@ -1,21 +1,45 @@
 const router = require('express').Router()
-const {Club, Poll, Option} = require('../db/models')
+const {Club, Poll, Option, Vote} = require('../db/models')
 module.exports = router
 
 // GET /api/clubs/:clubId/polls
-router.get('/:clubId/polls', async (req, res, next) => {
+// router.get('/:clubId/polls', async (req, res, next) => {
+//   try {
+//     // Poll.findAll(... )
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
+// GET /api/clubs/:clubId/polls/:pollId
+router.get('/:clubId/polls/:pollId', async (req, res, next) => {
   try {
-    // Poll.findAll(... )
+    const id = req.params.pollId
+    const poll = await Poll.findById(id)
+    res.json(poll)
   } catch (err) {
     next(err)
   }
 })
 
-// GET /api/clubs/:clubId/polls/:pollId
-router.get('/:clubId/polls/:pollId', async (req, res, next) => {
+router.put('/:clubId/polls/:pollId', async (req, res, next) => {
   try {
-    // Poll.findByPk(... )
+    //add security for req.user dont trust user!!!
+    const votes = req.body.votes
+    const pollId = req.params.pollId
+    votes.forEach(async vote => {
+      const existingVote = await Vote.findOne({
+        where: {optionId: vote, userId: 1, pollId}
+      })
+      if (existingVote) {
+        res.json('You already voted')
+      } else {
+        await Vote.create({optionId: vote, userId: 1, pollId})
+        res.json('You Voted!')
+      }
+    })
   } catch (err) {
+    console.log(err, 'ERROR ERROR')
     next(err)
   }
 })
