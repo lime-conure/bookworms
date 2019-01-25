fd457d12d65d50bbe799c50bf4fcb15bb2a8e1d6
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-01-25 16:34:37 -0500

    style: display due date on single poll page

--- server/db/models/vote/vote.js
+++ server/db/models/vote/vote.js
@@ -1,11 +1,5 @@
-const Sequelize = require('sequelize')
 const db = require('../../db')
 
-const Vote = db.define('vote', {
-  // id: {
-  //   type: Sequelize.INTEGER,
-  //   primaryKey: true
-  // }
-})
+const Vote = db.define('vote', {})
 
 module.exports = Vote
