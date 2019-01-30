437091b5a08da5616ca29d34f08b0822d78ec401
Sabira Davletshina <sabira.davletshina@gmail.com>
2019-01-30 16:32:02 -0500

    fix: fixed issues with route security

--- server/api/clubs.js
+++ server/api/clubs.js
@@ -37,14 +37,16 @@ router.post('/:clubId/deletemember', async (req, res, next) => {
     else {
       const clubId = req.params.clubId
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send('Not authorized')
-      else if (!club) {
+      if (!club) {
         res.status(404).send(`Club does not exist!`)
       } else {
-        await club.removeUser(req.user.id)
-        const updatedClub = await Club.findById(clubId)
-        res.send(updatedClub)
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send('Not authorized')
+        else {
+          await club.removeUser(req.user.id)
+          const updatedClub = await Club.findById(clubId)
+          res.send(updatedClub)
+        }
       }
     }
   } catch (err) {
@@ -59,11 +61,13 @@ router.get('/:clubId', async (req, res, next) => {
     else {
       const clubId = Number(req.params.clubId)
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
-      else if (!club) res.send('Club does not exist!')
+      if (!club) res.send('Club does not exist!')
       else {
-        res.json(club)
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          res.json(club)
+        }
       }
     }
   } catch (err) {
@@ -79,11 +83,14 @@ router.get('/:clubId/polls', async (req, res, next) => {
     else {
       const clubId = req.params.clubId
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
+      if (!club) res.send('Club does not exist!')
       else {
-        const polls = await Poll.findAll({where: {clubId: clubId}})
-        res.send(polls)
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          const polls = await Poll.findAll({where: {clubId: clubId}})
+          res.send(polls)
+        }
       }
     }
   } catch (err) {
@@ -98,77 +105,77 @@ router.post('/:clubId/polls', async (req, res, next) => {
     else {
       const clubId = req.params.clubId
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
+      if (!club) res.send('Club does not exist!')
       else {
-        const {
-          selectedBooks,
-          selectedDates,
-          selectedPlaces,
-          title,
-          dueDate,
-          notes
-        } = req.body
-        const newPoll = await Poll.create({title, dueDate, notes})
-        const books = []
-        for (let i = 0; i < selectedBooks.length; i++) {
-          const book = selectedBooks[i]
-          let existingBook = await Book.findOne({
-            where: {goodReadsId: book.goodReadsId}
-          })
-          if (!existingBook) {
-            existingBook = await Book.create(book)
-            let existingAuthor = await Author.findOne({
-              where: {goodReadsId: book.author.id}
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          const {
+            selectedBooks,
+            selectedDates,
+            selectedPlaces,
+            title,
+            dueDate,
+            notes
+          } = req.body
+          const newPoll = await Poll.create({title, dueDate, notes})
+          const books = []
+          for (let i = 0; i < selectedBooks.length; i++) {
+            const book = selectedBooks[i]
+            let existingBook = await Book.findOne({
+              where: {goodReadsId: book.goodReadsId}
             })
-            if (!existingAuthor) {
-              existingAuthor = await Author.create({
-                name: book.author.name,
-                goodReadsId: book.author.id
+            if (!existingBook) {
+              existingBook = await Book.create(book)
+              let existingAuthor = await Author.findOne({
+                where: {goodReadsId: book.author.id}
               })
+              if (!existingAuthor) {
+                existingAuthor = await Author.create({
+                  name: book.author.name,
+                  goodReadsId: book.author.id
+                })
+              }
+              existingBook.setAuthors([existingAuthor])
             }
-            existingBook.setAuthors([existingAuthor])
+            books.push(existingBook)
           }
-          books.push(existingBook)
+          await Promise.all(
+            books.map(book =>
+              Option.create({
+                type: 'book',
+                bookId: book.id,
+                bookName: book.title,
+                pollId: newPoll.id
+              })
+            )
+          )
+
+          await Promise.all(
+            selectedDates.map(date => {
+              console.log('date:', date)
+              const dateTime = new Date(date)
+              console.log('dateTime:', dateTime)
+              return Option.create({
+                type: 'time',
+                dateTime: dateTime,
+                pollId: newPoll.id
+              })
+            })
+          )
+
+          await Promise.all(
+            selectedPlaces.map(place =>
+              Option.create({
+                type: 'location',
+                location: place,
+                pollId: newPoll.id
+              })
+            )
+          )
+          newPoll.setClub(club)
+          res.send(newPoll)
         }
-        await Promise.all(
-          books.map(book =>
-            Option.create({
-              type: 'book',
-              bookId: book.id,
-              bookName: book.title,
-              pollId: newPoll.id
-            })
-          )
-        )
-
-        await Promise.all(
-          selectedDates.map(date => {
-            console.log('date:', date)
-            const dateTime = new Date(date)
-            console.log('dateTime:', dateTime)
-            return Option.create({
-              type: 'time',
-              dateTime: dateTime,
-              pollId: newPoll.id
-            })
-          })
-        )
-
-        await Promise.all(
-          selectedPlaces.map(place =>
-            Option.create({
-              type: 'location',
-              location: place,
-              pollId: newPoll.id
-            })
-          )
-        )
-
-        //const newPoll = await Poll.create({title, dueDate, notes})
-        const club = await Club.findById(req.params.clubId)
-        newPoll.setClub(club)
-        res.send(newPoll)
       }
     }
   } catch (err) {
@@ -183,34 +190,39 @@ router.get('/:clubId/polls/:pollId', async (req, res, next) => {
     else {
       const clubId = Number(req.params.clubId)
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
+      if (!club) res.send('Club does not exist!')
       else {
-        const pollId = Number(req.params.pollId)
-        const poll = await Poll.findById(pollId)
-        if (!poll) {
-          res.status(404).send(`Poll does not exist`)
-        } else {
-          const clubIdOfPoll = poll.getClubId()
-          if (clubId === clubIdOfPoll) {
-            // load poll options
-            const options = await poll.getOptions()
-
-            // count votes for each option
-            const allOptions = await Promise.all(
-              options.map(async option => {
-                const votes = await Vote.findAll({
-                  where: {optionId: option.id},
-                  attributes: ['userId']
-                })
-                return {option, votes, numVotes: votes.length}
-              })
-            )
-            res.json({poll, allOptions})
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          const pollId = Number(req.params.pollId)
+          const poll = await Poll.findById(pollId)
+          if (!poll) {
+            res.status(404).send(`Poll does not exist`)
           } else {
-            res
-              .status(403)
-              .send(`Not authorized: you can't vote for polls not in your club`)
+            const clubIdOfPoll = poll.getClubId()
+            if (clubId === clubIdOfPoll) {
+              // load poll options
+              const options = await poll.getOptions()
+
+              // count votes for each option
+              const allOptions = await Promise.all(
+                options.map(async option => {
+                  const votes = await Vote.findAll({
+                    where: {optionId: option.id},
+                    attributes: ['userId']
+                  })
+                  return {option, votes, numVotes: votes.length}
+                })
+              )
+              res.json({poll, allOptions})
+            } else {
+              res
+                .status(403)
+                .send(
+                  `Not authorized: you can't vote for polls not in your club`
+                )
+            }
           }
         }
       }
@@ -226,40 +238,45 @@ router.put('/:clubId/polls/:pollId', async (req, res, next) => {
     else {
       const clubId = Number(req.params.clubId)
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
+      if (!club) res.send('Club does not exist!')
       else {
-        const pollId = Number(req.params.pollId)
-        const votes = req.body.votes
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          const pollId = Number(req.params.pollId)
+          const votes = req.body.votes
 
-        // security: check if the clubId in route is equal to clubId of poll
-        const poll = await Poll.findById(pollId)
-        if (!poll) {
-          res.status(404).send(`Poll does not exist`)
-        } else {
-          const userId = req.user.id
-          const clubIdOfPoll = poll.getClubId()
-
-          if (clubId === clubIdOfPoll) {
-            const optionsInPoll = await poll.getOptions()
-            optionsInPoll.forEach(async option => {
-              const existingVote = await Vote.findOne({
-                where: {optionId: option.id, userId}
-              })
-              const voting = votes.includes(option.id)
-              if (!existingVote && voting) {
-                // adding a new vote
-                await Vote.create({optionId: option.id, userId})
-              } else if (existingVote && !voting) {
-                // undoing a vote
-                await Vote.destroy({where: {optionId: option.id, userId}})
-              }
-            })
-            res.json(votes)
+          // security: check if the clubId in route is equal to clubId of poll
+          const poll = await Poll.findById(pollId)
+          if (!poll) {
+            res.status(404).send(`Poll does not exist`)
           } else {
-            res
-              .status(403)
-              .send(`Not authorized: you can't vote for polls not in your club`)
+            const userId = req.user.id
+            const clubIdOfPoll = poll.getClubId()
+
+            if (clubId === clubIdOfPoll) {
+              const optionsInPoll = await poll.getOptions()
+              optionsInPoll.forEach(async option => {
+                const existingVote = await Vote.findOne({
+                  where: {optionId: option.id, userId}
+                })
+                const voting = votes.includes(option.id)
+                if (!existingVote && voting) {
+                  // adding a new vote
+                  await Vote.create({optionId: option.id, userId})
+                } else if (existingVote && !voting) {
+                  // undoing a vote
+                  await Vote.destroy({where: {optionId: option.id, userId}})
+                }
+              })
+              res.json(votes)
+            } else {
+              res
+                .status(403)
+                .send(
+                  `Not authorized: you can't vote for polls not in your club`
+                )
+            }
           }
         }
       }
@@ -276,18 +293,19 @@ router.get('/:clubId/messages', async (req, res, next) => {
     else {
       const clubId = Number(req.params.clubId)
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
-      else if (!club) {
-        res.status(404).send(`Club does not exist!`)
-      } else {
-        const messages = await Message.findAll({
-          where: {
-            clubId,
-            main: true
-          }
-        })
-        res.send(messages)
+      if (!club) res.send('Club does not exist!')
+      else {
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          const messages = await Message.findAll({
+            where: {
+              clubId,
+              main: true
+            }
+          })
+          res.send(messages)
+        }
       }
     }
   } catch (err) {
@@ -301,24 +319,25 @@ router.post('/:clubId/messages', async (req, res, next) => {
     else {
       const clubId = Number(req.params.clubId)
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
-      else if (!club) {
-        res.status(404).send(`Club does not exist!`)
-      } else {
-        await Message.create({
-          text: req.body.text,
-          userId: req.user.id,
-          clubId,
-          main: true
-        })
-        const messages = await Message.findAll({
-          where: {
+      if (!club) res.send('Club does not exist!')
+      else {
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          await Message.create({
+            text: req.body.text,
+            userId: req.user.id,
             clubId,
             main: true
-          }
-        })
-        res.send(messages)
+          })
+          const messages = await Message.findAll({
+            where: {
+              clubId,
+              main: true
+            }
+          })
+          res.send(messages)
+        }
       }
     }
   } catch (err) {
@@ -333,21 +352,22 @@ router.get('/:clubId/messages/:id', async (req, res, next) => {
     else {
       const clubId = Number(req.params.clubId)
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
-      else if (!club) {
-        res.status(404).send(`Club does not exist!`)
-      } else {
-        const message = await Message.findOne({
-          where: {id: req.params.id, main: true}
-        })
-        if (!message) res.status(404).send(`Message does not exist!`)
-        else if (message.threadId) {
+      if (!club) res.send('Club does not exist!')
+      else {
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          const message = await Message.findOne({
+            where: {id: req.params.id, main: true}
+          })
+          if (!message) res.status(404).send(`Message does not exist!`)
+          else if (message.threadId) {
             const threadMessages = await Message.findAll({
               where: {threadId: message.threadId}
             })
             res.send(threadMessages)
           } else res.send(message)
+        }
       }
     }
   } catch (err) {
@@ -362,39 +382,41 @@ router.post('/:clubId/messages/:id', async (req, res, next) => {
     else {
       const clubId = Number(req.params.clubId)
       const club = await Club.findById(clubId)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send(`Not authorized`)
-      else if (!club) {
-        res.status(404).send(`Club does not exist!`)
-      } else {
-        const message = await Message.findOne({where: {id: req.params.id}})
-        if (!message) res.status(404).send(`Message does not exist!`)
-        let thread = await Thread.findOne({where: {id: message.threadId}})
-        if (!thread) {
-          thread = await Thread.create({
-            name: message.text,
-            clubId: req.params.clubId
+      if (!club) res.send('Club does not exist!')
+      else {
+        const check = await club.hasUser(req.user.id)
+        if (!check) res.status(403).send(`Not authorized`)
+        else {
+          const message = await Message.findOne({where: {id: req.params.id}})
+          if (!message) res.status(404).send(`Message does not exist!`)
+          let thread = await Thread.findOne({where: {id: message.threadId}})
+          if (!thread) {
+            thread = await Thread.create({
+              name: message.text,
+              clubId: req.params.clubId
+            })
+            await message.setThread(thread.id)
+          }
+          await Message.create({
+            text: req.body.text,
+            userId: req.user.id,
+            clubId: message.clubId,
+            threadId: message.threadId,
+            main: false
           })
-          await message.setThread(thread.id)
+          const updatedThread = await Thread.findOne({
+            where: {id: thread.id},
+            include: [{model: Message, where: {main: false}}]
+          })
+          res.send(updatedThread)
         }
-        await Message.create({
-          text: req.body.text,
-          userId: req.user.id,
-          clubId: message.clubId,
-          threadId: message.threadId,
-          main: false
-        })
-        const updatedThread = await Thread.findOne({
-          where: {id: thread.id},
-          include: [{model: Message, where: {main: false}}]
-        })
-        res.send(updatedThread)
       }
     }
   } catch (err) {
     next(err)
   }
 })
+
 //GET /api/clubs/:clubId/join - join request
 router.get('/:clubId/join/:hash', async (req, res, next) => {
   try {
