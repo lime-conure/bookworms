37f5e30be36bec59b5dac814207a4bd1a5690a98
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-01-29 22:09:50 -0500

    style: all polls material UI

--- client/components/Polls.js
+++ client/components/Polls.js
@@ -2,54 +2,67 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchPolls} from '../store'
 import {NavLink, Link} from 'react-router-dom'
+import {withStyles} from '@material-ui/core/styles'
+import {Button, Typography, List, ListItem} from '@material-ui/core'
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 4,
+    paddingBottom: theme.spacing.unit * 2
+  }
+})
 
 class Polls extends Component {
-  // constructor() {
-  //   super()
-  // }
-
   componentDidMount() {
-    console.log('Polls did mount')
     this.props.fetchPolls(this.props.match.params.clubId)
   }
 
   render() {
     const polls = this.props.polls
-    console.log('Polls rendered')
+    const {classes} = this.props
     return (
-      <div>
-        <h2> All Polls:</h2>
-        <Link to={`/clubs/${this.props.match.params.clubId}/polls/create`}>
-          <button type="button"> Create a New Poll</button>
-        </Link>
-        <ul>
+      <main className={classes.root}>
+        <Typography variant="h2" gutterBottom color="primary">
+          All Polls
+        </Typography>
+
+        <List>
           {polls.map(poll => (
-            <div key={poll.id}>
-              <li>
-                <Link
-                  to={`/clubs/${this.props.match.params.clubId}/polls/${
-                    poll.id
-                  }`}
-                >
-                  <p>
-                    {poll.title}{' '}
-                    {poll.dueDate ? (
-                      <span>
-                        &ndash; <em>Ends on {poll.dueDate}</em>
-                      </span>
-                    ) : (
-                      ''
-                    )}
-                  </p>
-                </Link>
-              </li>
-            </div>
+            <ListItem
+              button
+              component={Link}
+              key={poll.id}
+              to={`/clubs/${this.props.match.params.clubId}/polls/${poll.id}`}
+            >
+              {poll.title}{' '}
+              {poll.dueDate ? (
+                <span>
+                  &ndash; <em>voting ends on {poll.dueDate}</em>
+                </span>
+              ) : (
+                ''
+              )}
+            </ListItem>
           ))}
-        </ul>
-      </div>
+        </List>
+        <Link to={`/clubs/${this.props.match.params.clubId}/polls/create`}>
+          <Button
+            type="button"
+            variant="contained"
+            color="primary"
+            size="large"
+          >
+            {' '}
+            Create a New Poll
+          </Button>
+        </Link>
+      </main>
     )
   }
 }
+
+const StyledPolls = withStyles(styles)(Polls)
 
 const mapState = state => ({
   polls: state.polls
@@ -58,4 +71,5 @@ const mapState = state => ({
 const mapDispatch = dispatch => ({
   fetchPolls: clubId => dispatch(fetchPolls(clubId))
 })
-export default connect(mapState, mapDispatch)(Polls)
+
+export default connect(mapState, mapDispatch)(StyledPolls)
