b5a98633255cff2587f977b827007615af13c965
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-01-31 12:07:05 -0500

    feat: thunk for adding book to club

--- client/store/clubBooks.js
+++ client/store/clubBooks.js
@@ -3,6 +3,7 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GET_CLUB_BOOKS = 'GET_CLUB_BOOKS'
+const ADD_CLUB_BOOK = 'ADD_CLUB_BOOK'
 
 /**
  * INITIAL STATE
@@ -15,6 +16,11 @@ const clubBooks = []
 const getClubBooks = books => ({
   type: GET_CLUB_BOOKS,
   books
+})
+
+const addClubBook = book => ({
+  type: ADD_CLUB_BOOK,
+  book
 })
 
 /**
@@ -30,10 +36,21 @@ export const fetchClubBooks = clubId => async dispatch => {
   }
 }
 
+export const postClubBook = (book, clubId) => async dispatch => {
+  try {
+    const {data} = await axios.post(`/api/clubs/${clubId}/books/add`, book)
+    dispatch(addClubBook(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 export default function(state = clubBooks, action) {
   switch (action.type) {
     case GET_CLUB_BOOKS:
       return action.books
+    case ADD_CLUB_BOOK:
+      return [...state.books, action.book]
     default:
       return state
   }
