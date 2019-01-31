4f1bfd55bfe02d597f11d143e813a5f47d808bcd
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-01-31 09:56:17 -0500

    feat: clubMembers reducer

---
+++ client/store/clubMembers.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+/**
+ * ACTION TYPES
+ */
+const GET_CLUB_MEMBERS = 'GET_CLUB_MEMBERS'
+
+/**
+ * INITIAL STATE
+ */
+const singleClub = {}
+
+/**
+ * ACTION CREATORS
+ */
+const getClubMembers = members => ({
+  type: GET_CLUB_MEMBERS,
+  members
+})
+
+/**
+ * THUNK CREATORS
+ */
+
+export const fetchClubMembers = clubId => async dispatch => {
+  try {
+    const {data} = await axios.get(`/api/clubs/${clubId}/users`)
+    dispatch(getClubMembers(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export default function(state = singleClub, action) {
+  switch (action.type) {
+    case GET_CLUB_MEMBERS:
+      return action.members
+    default:
+      return state
+  }
+}
