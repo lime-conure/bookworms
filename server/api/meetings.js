1ee7c6660b87ffed0cdd880b5a8b7c3bd4b2e072
Norka Avalos <norka.avalos11@gmail.com>
2019-02-01 12:30:27 -0500

    feat: create/get meetings backend

--- server/api/meetings.js
+++ server/api/meetings.js
@@ -5,14 +5,18 @@ module.exports = router
 //GET api/meetings - to find all meetings in one club
 router.get('/', async (req, res, next) => {
   try {
-    if (!req.user) res.status(403).send('Not authorized')
+    if (!req.body.userId) res.status(403).send('Not authorized')
+    if (!req.body.clubId) res.status(403).send('Not authorized')
     else {
-      const user = await User.findById(req.user.id)
-      const club = await Clubs.findById(req.club.id)
-      const check = await club.hasUser(req.user.id)
-      if (!check) res.status(403).send('Not authorized')
+      const user = await User.findById(req.body.userId)
+      const club = await Club.findById(req.body.clubId)
+      const check = await club.hasUser(user.id)
+      if (!check)
+        res
+          .status(403)
+          .send('You are not authorized to see the meetings for this club')
       else {
-        const meetings = await User.hasMeeting(user.id)
+        const meetings = await Meeting.findAll({where: {clubId: club.id}})
         res.json(meetings)
       }
     }
