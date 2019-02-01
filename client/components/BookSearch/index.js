2525184b3ce0f26d2a140e276112552af6ae185f
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-02-01 16:13:45 -0500

    chore: remove commented out code

--- client/components/BookSearch/index.js
+++ client/components/BookSearch/index.js
@@ -3,17 +3,6 @@ import BookList from './BookList'
 import Search from './Search'
 import BookResults from './BookResults'
 
-// Material UI
-import {withStyles} from '@material-ui/core/styles'
-import IconButton from '@material-ui/core/IconButton'
-import Icon from '@material-ui/core/Icon'
-import Tooltip from '@material-ui/core/Tooltip'
-import Typography from '@material-ui/core/Typography'
-
-const styles = theme => ({
-  root: {}
-})
-
 class BookSearch extends Component {
   constructor() {
     super()
@@ -21,7 +10,6 @@ class BookSearch extends Component {
       results: []
     }
     this.setResults = this.setResults.bind(this)
-    // this.addBook = this.addBook.bind(this)
   }
 
   setResults = results => {
@@ -41,4 +29,4 @@ class BookSearch extends Component {
   }
 }
 
-export default withStyles(styles)(BookSearch)
+export default BookSearch
