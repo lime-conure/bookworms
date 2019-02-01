20d66e084eea9ad585f8ba5c342a317307f49b9c
Norka Avalos <norka.avalos11@gmail.com>
2019-02-01 14:45:21 -0500

    feat: create/get meetings backend fixed

---
+++ client/store/meetings.js
@@ -0,0 +1,30 @@
+// import axios from 'axios';
+
+// /**
+//  * ACTION TYPES
+//  */
+//  const GET_MEETINGS = 'GET_MEETINGS'
+
+//  /**
+//  * INITIAL STATE
+//  */
+// const meetings = []
+
+// /**
+//  * ACTION CREATORS
+//  */
+
+//  const getMeetings = allmeetings => ({
+//    type: GET_MEETINGS,
+//    allmeetings
+//  });
+
+//  /**
+//  * THUNK CREATORS
+//  */
+
+//  export const fetchMeetings = () => async dispatch => {
+//    try{
+//      const {data} = await axios.get(`/api/clubs/meetings`)
+//    }
+//  }
