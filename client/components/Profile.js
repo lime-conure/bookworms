c638090e033b43072b18f3c11e3a3055498c9880
Jing Lu <jl201835@gmail.com>
2019-02-03 00:36:15 -0500

    feat: user profile

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -8,23 +8,38 @@ import Paper from '@material-ui/core/Paper'
 import Divider from '@material-ui/core/Divider'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import Icon from '@material-ui/core/Icon'
+import Grid from '@material-ui/core/Grid'
+import Avatar from '@material-ui/core/Avatar'
+import ButtonBase from '@material-ui/core/ButtonBase'
 
 const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
+    flexGrow: 1,
     padding: theme.spacing.unit * 4,
     marginTop: theme.spacing.unit * 16,
     marginLeft: theme.spacing.unit * 24,
     marginRight: theme.spacing.unit * 24
   },
-  button: {
-    marginTop: theme.spacing.unit * 4
+  bigAvatar: {
+    margin: 60,
+    width: 150,
+    height: 150
   }
+  // button: {
+  //   marginTop: theme.spacing.unit * 4,
+  //   marginBottom: theme.spacing.unit * 4
+  // },
+  // image: {
+  //   width: 200,
+  //   height: 200
+  // },
+  // img: {
+  //   margin: 'auto',
+  //   display: 'block',
+  //   maxWidth: '100%',
+  //   maxHeight: '100%'
+  //
 })
 
 class Profile extends Component {
@@ -33,14 +48,36 @@ class Profile extends Component {
     const user = this.props.user
     return (
       <Paper className={classes.root} elevation={2}>
-        <div>
+        <Grid container spacing={40} justify="center" alignItems="center">
+          {/* <Grid item>
+            <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="complex" src={user.imageUrl} />
+            </ButtonBase>
+					</Grid> */}
+          <Avatar
+            alt={user.fullName}
+            src={user.imageUrl}
+            className={classes.bigAvatar}
+          />
+          <Grid item>
+            <Typography gutterBottom variant="h5">
+              {user.fullName}
+            </Typography>
+            <Typography gutterBottom variant="h5">
+              {user.email}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Divider />
+        <UserBooks />
+        {/* <div>
           <img src={user.imageUrl} alt={user.fullName} />
           <Typography variant="h3" component="h3" gutterBottom>
             {user.fullName}
           </Typography>
           <Divider />
           <UserBooks />
-        </div>
+        </div> */}
       </Paper>
     )
   }
