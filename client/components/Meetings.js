cda777f5c0f8786c469f0d7c1cdbde0b9a62b517
Norka Avalos <norka.avalos11@gmail.com>
2019-02-03 21:18:18 -0500

    feat: meetings front end done

--- client/components/Meetings.js
+++ client/components/Meetings.js
@@ -2,67 +2,57 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchMeetings} from '../store/meetings'
 
+import {withStyles} from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-import Icon from '@material-ui/core/Icon'
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 752
-  },
-  demo: {
-    backgroundColor: theme.palette.background.paper
-  },
-  title: {
-    margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`
-  }
-})
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 
 class Meetings extends Component {
-  state = {
-    dense: false,
-    secondary: false
-  }
   componentDidMount() {
     const clubId = this.props.match.params.clubId
     this.props.fetchMeetings(clubId)
   }
   render() {
-    let meetings = this.props.meetings
-    console.log(meetings[0], '****')
+    const meetings = this.props.meetings
     return (
       <div>
         <Typography variant="h3" color="primary">
           Meetings
         </Typography>
-        <Divider />
-
-        <List>
-          {meetings.length
-            ? meetings.map(meeting => (
-                <ListItem key={meeting.id}>
-                  <ListItemText>
-                    <Typography variant="h5" color="secondary">
-                      {meeting.name.toUpperCase()}
-                      <br />
-                    </Typography>
-                    <Typography variant="h6">
-                      <Icon>location</Icon> Location: {meeting.location}
-                      <br />
-                      Date: {meeting.date.slice(0, 10)}
-                      <br />
-                      Time: {meeting.date.slice(11, 16)}
-                      <br />
-                    </Typography>
-                  </ListItemText>
-                </ListItem>
-              ))
-            : ''}
-        </List>
+        <br />
+        <Paper elevation={2}>
+          <Divider />
+          <Grid container spacing={40}>
+            <Grid item>
+              <List>
+                {meetings.length
+                  ? meetings.map(meeting => (
+                      <ListItem key={meeting.id}>
+                        <ListItemText>
+                          <Typography variant="h6" color="secondary">
+                            {meeting.name.toUpperCase()}
+                            <br />
+                          </Typography>
+                          <Typography variant="subtitle1">
+                            Location: {meeting.location}
+                            <br />
+                            Date: {meeting.date.slice(0, 10)}
+                            <br />
+                            Time: {meeting.date.slice(11, 16)}
+                            <br />
+                          </Typography>
+                        </ListItemText>
+                      </ListItem>
+                    ))
+                  : ''}
+              </List>
+            </Grid>
+          </Grid>
+        </Paper>
       </div>
     )
   }
