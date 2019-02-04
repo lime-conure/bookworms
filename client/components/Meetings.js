222f922870c46c47c8fe6cfe5f5c0ee7f74ed031
Norka Avalos <norka.avalos11@gmail.com>
2019-02-03 20:33:28 -0500

    meetings front end

--- client/components/Meetings.js
+++ client/components/Meetings.js
@@ -7,30 +7,56 @@ import Divider from '@material-ui/core/Divider'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-import {ClubMembers} from './ClubMembers'
+import Icon from '@material-ui/core/Icon'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper
+  },
+  title: {
+    margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`
+  }
+})
 
 class Meetings extends Component {
+  state = {
+    dense: false,
+    secondary: false
+  }
   componentDidMount() {
     const clubId = this.props.match.params.clubId
     this.props.fetchMeetings(clubId)
   }
   render() {
     let meetings = this.props.meetings
+    console.log(meetings[0], '****')
     return (
       <div>
         <Typography variant="h3" color="primary">
           Meetings
         </Typography>
         <Divider />
+
         <List>
           {meetings.length
             ? meetings.map(meeting => (
                 <ListItem key={meeting.id}>
                   <ListItemText>
-                    <Typography variant="h5">
-                      {meeting.name}
-                      {meeting.location}
-                      {meeting.date}
+                    <Typography variant="h5" color="secondary">
+                      {meeting.name.toUpperCase()}
+                      <br />
+                    </Typography>
+                    <Typography variant="h6">
+                      <Icon> location</Icon> Location: {meeting.location}
+                      <br />
+                      Date: {meeting.date.slice(0, 10)}
+                      <br />
+                      Time: {meeting.date.slice(11, 19)}
+                      <br />
                     </Typography>
                   </ListItemText>
                 </ListItem>
