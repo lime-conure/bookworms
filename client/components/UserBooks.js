48a43272de97bf9aee8ea50b6a5a8583f1b6fd69
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-02-04 21:55:47 -0500

    style: profile

--- client/components/UserBooks.js
+++ client/components/UserBooks.js
@@ -13,6 +13,9 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 4,
     marginBottom: theme.spacing.unit * 4,
     width: 720
+  },
+  sectionHeader: {
+    marginBottom: theme.spacing.unit * 3
   }
 })
 
@@ -58,7 +61,12 @@ class UserBooks extends Component {
   renderBookSection(books, type, classes) {
     return (
       <div className={classes.bookSection}>
-        <Typography variant="h4" component="h4" gutterBottom>
+        <Typography
+          variant="h5"
+          component="h5"
+          gutterBottom
+          className={classes.sectionHeader}
+        >
           {type === 'now'
             ? `Books I'm Reading`
             : type === 'future' ? `Books I Want To Read` : `Books I've Read`}
