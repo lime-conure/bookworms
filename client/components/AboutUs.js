4aec72c4f12063366fd231053d06698ab59b6790
Norka Avalos <norka.avalos11@gmail.com>
2019-02-09 12:20:55 -0500

    feat: about us page

--- client/components/AboutUs.js
+++ client/components/AboutUs.js
@@ -42,6 +42,49 @@ class AboutUs extends Component {
         <Typography variant="h6" component="h6">
           "Our goal is to help you organize your reading life"
         </Typography>
+        <br />
+        <Typography variant="h4" component="h4">
+          Our Team
+        </Typography>
+
+        <Grid
+          container
+          spacing={32}
+          justify="space-between"
+          alignItems="flex-start"
+        >
+          <Grid item md={3} className={classes.userInfo}>
+            <Avatar
+              alt="Brynn Shepherd"
+              src="public/teamPictures/Brynn_Shepherd.jpg"
+              className={classes.bigAvatar}
+            />
+          </Grid>
+
+          <Grid item md={3} className={classes.userInfo}>
+            <Avatar
+              alt="Jing Lu"
+              src="/public/teamPictures/Jing_Lu.jpg"
+              className={classes.bigAvatar}
+            />
+          </Grid>
+
+          <Grid item md={3} className={classes.userInfo}>
+            <Avatar
+              alt="Norka Avalos"
+              src="/public/teamPictures/Norka_Avalos.jpg"
+              className={classes.bigAvatar}
+            />
+          </Grid>
+
+          <Grid item md={3} className={classes.userInfo}>
+            <Avatar
+              alt="Sabira Davletshina"
+              src="https://www.linkedin.com/in/sabira-davletshina-77a104179/detail/photo/"
+              className={classes.bigAvatar}
+            />
+          </Grid>
+        </Grid>
       </Paper>
     )
   }
